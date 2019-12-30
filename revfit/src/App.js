e3175e4752adc4fc96919379e746aed4be64d68a
Harvey25Edward <you@example.com>
2019-12-30 16:44:10 -0500

    youtube first trial

---
+++ revfit/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactYoutubeExample from './Youtubeexapl/ReactYoutubeExample'
+class App extends Component{
+ render(){
+  return (
+    <div className="App">
+      <ReactYoutubeExample videoId='dhYOPzcsbGM'/>
+    </div>
+  );
+}
+}
+export default App;
---
+++ revfit/src/Youtubeexapl/ReactYoutubeExample.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import YouTube from 'react-youtube'
+
+
+class ReactYouTubeExample extends Component {
+  videoOnReady (event) {
+    // access to player in all event handlers via event.target
+    // event.target.playVideoAt(50) // 50 seconds
+    const player = event.target
+    event.target.playVideo()
+  
+   
+    console.log(event.target)
+  }
+  
+  render () {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    }
+    const {videoId} = this.props
+    return (
+      <YouTube
+        videoId={videoId}
+        opts={opts}
+        onReady={this.videoOnReady}
+       
+        
+      />
+    )
+  }
+}
+
+export default ReactYouTubeExample
