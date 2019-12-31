59ee11e97cf0f4dafa1d82228839edfde325e094
Harvey25Edward <you@example.com>
2019-12-31 13:53:21 -0500

    dependencies installed

--- revfit/src/App.tsx
+++ revfit/src/App.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Exam } from './Youtubeexapl/ReactYoutubeExampleComponent';
+import TabsWrappedLabel from './Tab-Component/TabComponent';
+import DisabledTabs from './Tab-Component/TabComponent';
+import PersistentDrawerLeft from './Tab-Component/TabComponent';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <PersistentDrawerLeft/>
         <Exam/>
       </header>
     </div>
