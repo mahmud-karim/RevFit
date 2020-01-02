59b964c1fe516162fb9d66f9c41d7bcba83df68e
mahmud <mahmudskarim@gmail.com>
2020-01-02 17:56:35 -0500

    started the homecomponent

---
+++ revfit/src/components/home-component/HomeComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export class HomeComponent extends React.Component<any, any>{
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
