5ac08587ca7781567956baebb621b83742b493fa
mahmud <mahmudskarim@gmail.com>
2020-01-03 16:18:16 -0500

    centered appbar and removed navbar

--- revfit/src/components/appbar-component/AppbarComponent.tsx
+++ revfit/src/components/appbar-component/AppbarComponent.tsx
@@ -17,7 +17,11 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import { Link } from 'react-router-dom';
+import { BottomNavigationAction } from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 const drawerWidth = 240;
 
@@ -27,6 +31,7 @@ const useStyles = makeStyles((theme: Theme) =>
             display: 'flex',
         },
         appBar: {
+            alignItems: 'center',
             transition: theme.transitions.create(['margin', 'width'], {
                 easing: theme.transitions.easing.sharp,
                 duration: theme.transitions.duration.leavingScreen,
@@ -41,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
             }),
         },
         menuButton: {
-            marginRight: theme.spacing(2),
+            marginRight: theme.spacing(10),
         },
         hide: {
             display: 'none',
@@ -114,7 +119,10 @@ export default function AppbarComponent() {
                     <Typography variant="h6" noWrap>
                         RevFit
                     </Typography>
-                    <Link to="/youtube">YouTube</Link>
+                    <BottomNavigationAction className="appbar" label="Home" icon={<HomeIcon />} />
+                    <BottomNavigationAction className="appbar" label="Recipes" icon={<MenuBookIcon />} />
+                    <BottomNavigationAction className="appbar" label="Progress" icon={<BarChartIcon />} />
+                    <BottomNavigationAction className="appbar" label="Me" icon={<AccountBoxIcon />} />
                 </Toolbar>
             </AppBar>
             <Drawer
@@ -156,29 +164,6 @@ export default function AppbarComponent() {
                 })}
             >
                 <div className={classes.drawerHeader} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-        </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
             </main>
         </div>
     );
