9ffbc8d2955c9aa016ac1c985608280e831cfc40
Harvey25Edward <you@example.com>
2020-01-03 14:28:34 -0500

    just commit

--- revfit/src/components/tab-component/WeekOneComponent.tsx
+++ revfit/src/components/tab-component/WeekOneComponent.tsx
@@ -1,10 +1,10 @@
-import React from './node_modules/react';
-import { makeStyles, Theme } from './node_modules/@material-ui/core/styles';
-import AppBar from './node_modules/@material-ui/core/AppBar';
-import Tabs from './node_modules/@material-ui/core/Tabs';
-import Tab from './node_modules/@material-ui/core/Tab';
-import Typography from './node_modules/@material-ui/core/Typography';
-import Box from './node_modules/@material-ui/core/Box';
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import { Exam } from '../youtubeexapl/DayOneComponent';
 import { DayTwo } from '../youtubeexapl/DayTwoComponent';
 import { DayThree } from '../youtubeexapl/DayThreeComponent';
