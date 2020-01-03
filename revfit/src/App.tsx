6e21e34582a254caa915562b7e39d32957ab0eef
mahmud <mahmudskarim@gmail.com>
2020-01-03 12:38:55 -0500

    got rid of some stuff

--- revfit/src/App.tsx
+++ revfit/src/App.tsx
@@ -5,8 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { store } from './Store';
 import LoginComponent from './components/login-component/LoginContainer'
 import { HomeComponent } from './components/home-component/HomeComponent';
-import ScrollableTabsButtonAuto from './component/Tab-Component/WeekOneComponent';
-import WeekTwo from './component/Tab-Component/WeekTwoComponent';
 
 const App: React.FC = () => {
   return (
@@ -15,8 +13,6 @@ const App: React.FC = () => {
         <Router>
           <Switch>
             <Route path='/home' component={HomeComponent} />
-            <Route path='/WeekOne' component={ScrollableTabsButtonAuto} />
-            <Route path='/WeekTwo' component={WeekTwo} />
             <Route path='/login' component={LoginComponent} />
             <Route path='/' component={LoginComponent} />
           </Switch>
